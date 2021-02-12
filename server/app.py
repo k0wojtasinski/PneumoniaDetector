cf20aa6a9b051636504eacb95cdade10e1709d60
Kacper <k0wojtasinski@gmail.com>
2021-02-12 10:33:38 +0000

    Added second model

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
-from fastapi import FastAPI, File, UploadFile, Request
+from fastapi import FastAPI, File, UploadFile, Request, HTTPException, status
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -8,34 +8,49 @@ import numpy as np
 
 import requests
 
-templates = Jinja2Templates(directory='server/templates')
+templates = Jinja2Templates(directory="server/templates")
+
 
 async def preprocess_image_to_array(file: File, target_size=(256, 256)):
     content = await file.read()
-    
-    with open('server/static/processed_image.jpg', 'wb') as _f:
+
+    with open("server/static/processed_image.jpg", "wb") as _f:
         _f.write(content)
 
     image = load_img(
-        "server/static/processed_image.jpg", target_size=target_size, 
+        "server/static/processed_image.jpg",
+        target_size=target_size,
     )
 
     arr = img_to_array(image) / 255
     return np.expand_dims(arr, 0)
 
+
 app = FastAPI()
 
 app.mount("/static/", StaticFiles(directory="server/static/"), name="static")
 
-@app.post("/process_xray/")
-async def create_upload_file(file: UploadFile = File(...)):
+
+@app.post("/process_xray/{model_type}")
+async def create_upload_file(model_type: str, file: UploadFile = File(...)):
+    if model_type not in ("xray", "transferlearning"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"{model_type} model type is not available",
+        )
+
     arr = await preprocess_image_to_array(file)
 
-    data = {'instances': arr.tolist()}
-    res = requests.post('http://tensorflow_serving:8501/v1/models/xray:predict', json=data)
+    data = {"instances": arr.tolist()}
+    res = requests.post(
+        f"http://tensorflow_serving:8501/v1/models/{model_type}:predict", json=data
+    )
 
-    return res.json()
+    predictions = res.json()["predictions"]
+
+    return predictions[0][-1]
+
 
 @app.get("/", response_class=HTMLResponse)
 async def show_index(request: Request):
-    return templates.TemplateResponse("index.html", context={'request': request})
+    return templates.TemplateResponse("index.html", context={"request": request})
